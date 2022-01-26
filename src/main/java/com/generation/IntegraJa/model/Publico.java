22fd64ef43f279b672a981ad9853538c50ff2ab3
Cesar <rasec.desenvolvedor@gmail.com>
2022-01-26 14:54:50 -0300

    @1-Cesar [atributos e getters/setters de model publico]

---
+++ src/main/java/com/generation/IntegraJa/model/Publico.java
@@ -0,0 +1,59 @@
+package com.generation.IntegraJa.model;
+
+/**@author Cesar
+ * @version v1
+ * @since 26/01/2022 
+ * */
+
+public class Publico {
+
+	private long idPublico;
+	
+	private Boolean lgbtqiapPublico;
+	
+	private Boolean mulheresPublico;
+	
+	private Boolean pcdsPublico;
+	
+	private Boolean indigenasPublico;
+
+	public long getIdPublico() {
+		return idPublico;
+	}
+
+	public void setIdPublico(long idPublico) {
+		this.idPublico = idPublico;
+	}
+
+	public Boolean getLgbtqiapPublico() {
+		return lgbtqiapPublico;
+	}
+
+	public void setLgbtqiapPublico(Boolean lgbtqiapPublico) {
+		this.lgbtqiapPublico = lgbtqiapPublico;
+	}
+
+	public Boolean getMulheresPublico() {
+		return mulheresPublico;
+	}
+
+	public void setMulheresPublico(Boolean mulheresPublico) {
+		this.mulheresPublico = mulheresPublico;
+	}
+
+	public Boolean getPcdsPublico() {
+		return pcdsPublico;
+	}
+
+	public void setPcdsPublico(Boolean pcdsPublico) {
+		this.pcdsPublico = pcdsPublico;
+	}
+
+	public Boolean getIndigenasPublico() {
+		return indigenasPublico;
+	}
+
+	public void setIndigenasPublico(Boolean indigenasPublico) {
+		this.indigenasPublico = indigenasPublico;
+	}
+}
