ac63d0e1054eeecda1e6538b0e0faf0f69996a56
Edgar Soares <edemarinho@outlook.com>
2022-01-26 10:25:51 -0300

    @oieusouoede [Criação da classe UsuarioController]

---
+++ src/main/java/com/generation/IntegraJa/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.generation.IntegraJa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.IntegraJa.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin("*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	
+
+}
