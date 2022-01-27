b7ae5dac62a1b3935522e38cf011033759b67bc6
Pedro Lucas <peedrolucass@outlook.com>
2022-01-27 11:21:16 -0300

    @pdrlcx [Repository Postagem]

---
+++ src/main/java/com/generation/IntegraJa/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.generation.IntegraJa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.IntegraJa.model.Postagem;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List<Postagem> findAllByTituloPostContainingIgnoreCase (String tituloPost);
+}
