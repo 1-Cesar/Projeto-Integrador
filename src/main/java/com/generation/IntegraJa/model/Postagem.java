b91282b00249118dddc41b7094745daba6790d47
Edgar Soares <edemarinho@outlook.com>
2022-01-27 12:19:18 -0300

    @oieusouoede [Correção: marcações de stashed changes]

--- src/main/java/com/generation/IntegraJa/model/Postagem.java
+++ src/main/java/com/generation/IntegraJa/model/Postagem.java
@@ -1,77 +1,66 @@
 package com.generation.IntegraJa.model;
 
 import java.util.Date;
-<<<<<<< Updated upstream
 
-=======
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
->>>>>>> Stashed changes
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< Updated upstream
-=======
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
->>>>>>> Stashed changes
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-<<<<<<< Updated upstream
-=======
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
->>>>>>> Stashed changes
+
 import com.generation.IntegraJa.model.enums.Tema;
 
 /**
  * @author Edgar
  * @version v2
- * @since 27/01/2022 
- * */
+ * @since 27/01/2022
+ */
 
-<<<<<<< Updated upstream
-=======
 @Entity
 @Table(name = "tb_postagem")
->>>>>>> Stashed changes
 public class Postagem {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idPost;
-	
+
 	@NotNull
-	@Size(min = 5,  max = 180)
+	@Size(min = 5, max = 180)
 	private String tituloPost;
-	
+
 	@NotNull
 	@Size(min = 20, max = 1000)
 	private String textoPost;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataPost = new java.sql.Date(System.currentTimeMillis());
-	
+
 	@Size(min = 10, max = 255)
 	private String imagemPost;
-	
+
 	@NotNull
 	private Long likesPost;
-	
+
 	@Enumerated(EnumType.STRING)
 	private Tema tema;
-	
+
 	@ManyToOne
 	@JsonIgnoreProperties("postagens")
 	private Usuario usuario;
-	
+
 	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("post")
 	private List<Publico> publico;
@@ -147,5 +136,5 @@ public class Postagem {
 	public void setPublico(List<Publico> publico) {
 		this.publico = publico;
 	}
-		
+
 }
