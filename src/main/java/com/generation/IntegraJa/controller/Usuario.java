385b9846cebb0eea86926ea68096f0358853d343
Edilaine <edilaine45@yahoo.com.br>
2022-01-27 09:18:07 -0300

    @Edilainesds [Add ao Controler metodo Put e Delete]

--- src/main/java/com/generation/IntegraJa/controller/Usuario.java
+++
@@ -1,5 +0,0 @@
-package com.generation.IntegraJa.controller;
-
-public class Usuario {
-
-}
--- src/main/java/com/generation/IntegraJa/controller/UsuarioController.java
+++ src/main/java/com/generation/IntegraJa/controller/UsuarioController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,15 @@ public class UsuarioController {
 	@PostMapping
 	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
