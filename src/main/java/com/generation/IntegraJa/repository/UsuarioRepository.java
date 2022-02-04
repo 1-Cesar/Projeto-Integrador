ff751ba1cceecf7ddb5325c0921565c0876a1db1
pdrlcx <peedrolucass@outlook.com>
2022-02-04 17:12:19 -0300

    @pdrlcx [Cria o UserDetailsServiceImpl]

--- src/main/java/com/generation/IntegraJa/repository/UsuarioRepository.java
+++ src/main/java/com/generation/IntegraJa/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.generation.IntegraJa.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -18,5 +19,5 @@ import com.generation.IntegraJa.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	public List<Usuario> findAllByNomeUsuarioContainingIgnoreCase (String nomeUsuario);
-
+	public Optional<Usuario> findByEmailUsuario(String emailUsuario);
 }
